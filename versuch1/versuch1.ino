dd2dbe1cc9958952cdb1d54bae4884a651eff71c
Skzter <v.goetze03@gmail.com>
2024-01-04 15:51:21 +0100

    dateien schieben

--- versuch1/versuch1.ino
+++ versuch1/versuch1.ino
@@ -1,14 +1,19 @@
 #include <DmxSimple.h>
-#include "/home/vincent/functions.h"
-
-unsigned long zeit;
-double verz = 517.14;
+#include "functions.h"
 
 void setup()
 {
   DmxSimple.usePin(3);
   DmxSimple.maxChannel(44);
   Serial.begin(9600);
+
+  farbeRGB(255,0,0,1);
+  farbeRGB(255,0,0,4);
+  farbeRGB(0,0,255,2);
+  farbeRGB(0,0,255,3); 
+
+  drehen(180, 5, 100, 2);
+  drehen(180, 5, 100,3);
 
   
   
