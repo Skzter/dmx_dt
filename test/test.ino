10e0db942b3fa4ead9b4431e9f33fbf5aab7b1a0
Brubi25 <148341586+Brubi25@users.noreply.github.com>
2024-01-04 17:07:48 +0100

    Update test.ino
    minor changes

--- test/test.ino
+++ test/test.ino
@@ -1,6 +1,23 @@
 #include"functions.h"
 
 double nextBeat(double);
+void n_blinken(int, double&);
+
+void n_blinken(int n, double& start){
+for (int i = 0; i < n; i++){
+    Serial.println(i);
+    dimmen(100,1);
+    delay(100);
+    dimmen(0,1);
+    start = nextBeat(start);
+    dimmen(100,4);
+    delay(100);
+    dimmen(0,4);
+    start = nextBeat(start);
+  }
+
+
+}
 
 double nextBeat(double time){
   while (millis() < time + 517.24){
@@ -8,23 +25,47 @@ double nextBeat(double time){
   }
   return time + 517.24;
 }
+
+
 void setup() {
-double start = millis();
-farbeRGB(255,0,200,2);
+DmxSimple.maxChannel(44);
+Serial.begin(9600);
+
+for(int i = 3; i > 0; i--){
+  Serial.println(i);
+  delay(1000);
+}
+  Serial.println("START!");
+  delay(517*4);
+  double start = millis();
 
 
-//synchrones Nicken
+
+drehen(0,150,100,2);
+drehen(0,150,100,3);
+farbeRGB(255,0,0,2);
+farbeRGB(0,0,255,3);
+dimmen(0,2);
+dimmen(0,3);
+white(1);
+white(4);
+
 
 while(true){
-drehen(180,30,100,2);
-delay(200);
-dimmen(100,2);
-drehen(180,0,100,2);
-delay(50);
-dimmen(0,2);
-start = nextBeat(start);
-}
-
+    n_blinken(7, start);
+    dimmen(100,1);
+    dimmen(100,2);
+    dimmen(100,3);
+    dimmen(100,4);
+    delay(100);
+    dimmen(0,1);
+    dimmen(0,4);
+    delay(200);
+    dimmen(0,2);
+    dimmen(0,3);
+    
+    start = nextBeat(start);
+  }
 }
 
 void loop() {
